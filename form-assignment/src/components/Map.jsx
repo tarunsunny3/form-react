0e667bfb0ff2cc696a2feb2dcfc7f3abde4cb966
Tarun Chinthakindi <tarunsunny3@gmail.com>
2021-09-30 18:57:28 +0000

    added .env

--- form-assignment/src/components/Map.jsx
+++ form-assignment/src/components/Map.jsx
@@ -27,8 +27,8 @@ import {
               <MapWithAMarker
               latitude={lat}
               longitude ={lng}
-            //   AIzaSyD2LiMRSs7Zcls5ZspW3Qrd8LpZikWuCxI
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2LiMRSs7Zcls5ZspW3Qrd8LpZikWuCxI&v=3.exp&libraries=geometry,drawing,places"
+            
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
     loadingElement={<div style={{ height: `100%` }} />}
     containerElement={<div style={{ height: `400px` }} />}
     mapElement={<div style={{ height: `100%` }} />}
